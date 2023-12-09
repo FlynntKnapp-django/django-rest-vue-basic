fb0f90fb4a5b683d5d0b9ba3c3fc60d6314e961d
FlynntKnapp <bruce.stull@gmail.com>
2023-12-09 18:31:40 -0500

    In progress.

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from api import views
@@ -7,4 +8,7 @@ app_name = "api"
 router = DefaultRouter()
 router.register("things", views.ThingViewSet, basename="things")
 
-urlpatterns = router.urls + []
+# Add a named URL pattern for the API root
+urlpatterns = [
+    path("", router.get_api_root_view(), name="api-root"),
+] + router.urls
